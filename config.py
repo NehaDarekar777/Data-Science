782cda6bf432d027fa3d3aab13524ee8e968b6bb
Neha Darekar <138971615+NehaDarekar777@users.noreply.github.com>
2023-07-18 13:32:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 PORT_NUMBER=5000
 
-JSON_FILE_PATH= r'statics\json_file\proj_data.json'
+JSON_FILE_PATH= r'C:\NAINA\Velocity_06_03_23\Project _machine learning\01.project\neha\california\proj_data.json'
 
 MODEL_FILE_PATH = r"statics\model_file\Linear_regression.pkl"
