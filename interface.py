33d65a1ac0b1018c998e17dd3baa22ab032f15ec
Neha Darekar <138971615+NehaDarekar777@users.noreply.github.com>
2023-07-18 14:31:05 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,77 +1,37 @@
-from Flask import Flask,jsonify,request,render_template
+from flask import Flask, jsonify, request, render_template
 from utils import CaliforniaHouse
 import config
 
-
-app=Flask(__name__)
-
-
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"result":"Successfull"})
-
+    return jsonify({"result": "Successful"})
 
 @app.route('/calhouseprice')
 def home_api():
     print("California home API")
     return render_template('calhouseprice.html')
 
-
-@app.route('/predicted price',methods=['GET','POST'])
+@app.route('/predicted_price', methods=['GET', 'POST'])
 def predicted_price():
-    
-    if request.method=='GET':
-        data =request.args.get
-        print('Data:',data)
-        longitude             =float(data('longitude'))
-        latitude              =float(data('latitude'))
-        housing_median_age    =float(data('housing_median_age'))
-        total_rooms           =float(data('total_rooms'))
-        total_bedrooms        =float(data('total_bedrooms'))
-        population            =float(data('population'))
-        households            =float(data('households'))
-        median_income         =float(data('median_income'))
-        ocean_proximity       =data['ocean_proximity']
+    data = request.args.get() if request.method == 'GET' else request.form
 
+    longitude = float(data.get('longitude'))
+    latitude = float(data.get('latitude'))
+    housing_median_age = float(data.get('housing_median_age'))
+    total_rooms = float(data.get('total_rooms'))
+    total_bedrooms = float(data.get('total_bedrooms'))
+    population = float(data.get('population'))
+    households = float(data.get('households'))
+    median_income = float(data.get('median_income'))
+    ocean_proximity = data.get('ocean_proximity')
 
+    obj = CaliforniaHouse(longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households,
+                          median_income, ocean_proximity)
 
-        # longitude             =int(data['longitude'])
-        # latitude              =int(data['latitude'])
-        # housing_median_age    =int(data['housing_median_age'])
-        # total_rooms           =int(data['total_rooms'])
-        # total_bedrooms        =int(data['total_bedrooms'])
-        # population            =int(data['population'])
-        # households            =int(data['households'])
-        # median_income         =int(data['median_income'])
-        # ocean_proximity       =data['ocean_proximity']
+    pchp = obj.price_prediction()
+    return render_template("calhouseprice.html", predict_price=pchp)
 
-        obj=CaliforniaHouse(longitude, latitude, housing_median_age, total_rooms,total_bedrooms, population, households, 
-                    median_income, ocean_proximity)
-
-        pchp=obj.price_prediction()
-        return render_template("calhouseprice.html",predict_price =pchp)
-    
-    elif request.method=='POST':
-        data =request.args.get
-        print('Data:',data)
-        longitude             =int(data('longitude'))
-        latitude              =int(data('latitude'))
-        housing_median_age    =int(data('housing_median_age'))
-        total_rooms           =int(data('total_rooms'))
-        total_bedrooms        =int(data('total_bedrooms'))
-        population            =int(data('population'))
-        households            =int(data('households'))
-        median_income         =int(data('median_income'))
-        ocean_proximity       =data['ocean_proximity']
-
-        obj=CaliforniaHouse(longitude, latitude, housing_median_age, total_rooms,total_bedrooms, population, households, 
-                    median_income, ocean_proximity)
-
-        pchp=obj.price_prediction()
-        return render_template("calhouseprice.html",predict_price =pchp)
-
-    return jsonify({"Message" : "Unsuccessful"})
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=config.PORT_NUMBER)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=config.PORT_NUMBER)
